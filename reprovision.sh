9b03629b642c8cfe69a86f13c27cb676df752663
Christoph Berg <myon@debian.org>
2020-06-25 21:11:46 +0200

    reprovision.sh: Don't die on first invocation

--- reprovision.sh
+++ reprovision.sh
@@ -1,14 +1,12 @@
 set -e
 set -x
 
-psql postgres -c "drop database gts";
+psql postgres -c "drop database gts" || :
 psql postgres -c "create database gts";
 
 psql gts -c "CREATE EXTENSION postgis; CREATE EXTENSION hstore;"
 
 osm2pgsql -c data/duesseldorf-regbez-latest.osm.pbf --number-processes=8 -j --hstore-add-index  --database=gts -C 2500 --merc
 #pg_restore --dbname gts data/osm.dump -j4 --verbose
-pushd sql
+cd sql
 psql gts < init.sql
-popd
-
