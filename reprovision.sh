91f1cb7a4f82058b8b02d17e914c96d735f55d32
Christoph Berg <christoph.berg@credativ.de>
2020-08-05 16:24:04 +0200

    Add comment where to get duesseldorf-regbez-latest.osm.pbf from

--- reprovision.sh
+++ reprovision.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 set -e
 set -x
 
@@ -6,7 +8,9 @@ psql postgres -c "create database gts";
 
 psql gts -c "CREATE EXTENSION postgis; CREATE EXTENSION hstore;"
 
+# OSM data from https://download.geofabrik.de/europe/germany/nordrhein-westfalen/duesseldorf-regbez.html
 osm2pgsql -c data/duesseldorf-regbez-latest.osm.pbf --number-processes=8 -j --hstore-add-index  --database=gts -C 2500 --merc
 #pg_restore --dbname gts data/osm.dump -j4 --verbose
+
 cd sql
 psql gts < init.sql
